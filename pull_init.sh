b7e5d909779fdb4bc78c4b08b7bb97d24c286e1d
Mateusz Wojczal <mateusz@wojczal.com>
2022-06-06 16:06:54 +0200

    caddy

--- pull_init.sh
+++ pull_init.sh
@@ -4,7 +4,7 @@ cd EscolaLMS
 rm -rf .git
 #./init.sh
 if $(nc -z -v -w5 localhost 80  2>&1 | grep -q 'succeeded'); then echo "Port 80 is not free. Exiting. ";exit 1; else echo "Port 80 is free"; fi;
-if $(nc -z -v -w5 localhost 1000  2>&1 | grep -q 'succeeded'); then echo "Port 1000 is not free. Exiting. ";exit 1; else echo "Port 1000 is free"; fi;
-if $(nc -z -v -w5 localhost 3000  2>&1 | grep -q 'succeeded'); then echo "Port 3000 is not free. Exiting. ";exit 1; else echo "Port 3000 is free"; fi;
+#if $(nc -z -v -w5 localhost 1000  2>&1 | grep -q 'succeeded'); then echo "Port 1000 is not free. Exiting. ";exit 1; else echo "Port 1000 is free"; fi;
+#if $(nc -z -v -w5 localhost 3000  2>&1 | grep -q 'succeeded'); then echo "Port 3000 is not free. Exiting. ";exit 1; else echo "Port 3000 is free"; fi;
 make init 
 make success
