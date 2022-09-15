ae132c39648a28b52015b3056d460147fef7480a
Mateusz Wojczal <mateusz@wojczal.com>
2022-09-15 20:00:31 +0200

    fix credentials

--- credentials.sh
+++ credentials.sh
@@ -10,13 +10,13 @@ ADMIN_URL="${ADMIN_URL:-http://admin.wellms.localhost}"
 APP_URL="${APP_URL:-http://app.wellms.localhost}"  
 MAILHOG_URL="${MAILHOG_URL:-http://mailhog.wellms.localhost}"  
 
+# fetch just domain from URLs 
 API_URL_DOMAIN="$(echo "$API_URL" | awk -F/ '{print $3}')"
 ADMIN_URL_DOMAIN="$(echo "$ADMIN_URL" | awk -F/ '{print $3}')"
 APP_URL_DOMAIN="$(echo "$APP_URL" | awk -F/ '{print $3}')"
 MAILHOG_URL_DOMAIN="$(echo "$MAILHOG_URL" | awk -F/ '{print $3}')"
 
-
-# create tmp yaml file 
+# create tmp yaml file to work on 
 cp docker-compose.yml.tpl docker-compose.yml.tpl.tmp
 YAML=$(docker run -i --rm mikefarah/yq eval '.services.postgres.environment[2] = "POSTGRES_PASSWORD='${DBPASS_RND}'"' < docker-compose.yml.tpl.tmp)
 echo "$YAML" > docker-compose.yml.tpl.tmp
@@ -26,7 +26,6 @@ YAML=$(docker run -i --rm mikefarah/yq eval '.services.app.environment[0] = "API
 echo "$YAML" > docker-compose.yml.tpl.tmp
 YAML=$(docker run -i --rm mikefarah/yq eval '.services.admin.environment[0] = "API_URL='${API_URL}'"' < docker-compose.yml.tpl.tmp)
 echo "$YAML" > docker-compose.yml.tpl.tmp
-
 
 # Remove previous files if the exists
 rm -f docker-compose.yml .env docker-compose.yml.tpl.tmp
