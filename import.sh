b2553f3ceec25e32db2c7d6434e87783f4642a3b
Mateusz Wojczal <mateusz@wojczal.com>
2022-09-22 16:31:59 +0200

    import script

--- import.sh
+++ import.sh
@@ -2,21 +2,24 @@
 . .env
 
 url_encode() {
-   awk 'BEGIN {
-      for (n = 0; n < 125; n++) {
-         m[sprintf("%c", n)] = n
-      }
-      n = 1
-      while (1) {
-         s = substr(ARGV[1], n, 1)
-         if (s == "") {
-            break
-         }
-         t = s ~ /[[:alnum:]_.!~*\47()-]/ ? t s : t sprintf("%%%02X", m[s])
-         n++
-      }
-      print t
-   }' "$1"
+    local S="${1}"
+    local encoded=""
+    local ch
+    local o
+    for i in $(seq 0 $((${#S} - 1)) )
+    do
+        ch=${S:$i:1}
+        case "${ch}" in
+            [-_.~a-zA-Z0-9]) 
+                o="${ch}"
+                ;;
+            *) 
+                o=$(printf '%%%02x' "'$ch")                
+                ;;
+        esac
+        encoded="${encoded}${o}"
+    done
+    echo ${encoded}
 }
 
 DB_PASSWORD_ESC=$(url_encode "$DB_PASSWORD")
