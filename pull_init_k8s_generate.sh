60c21b60481e5f2f581f36087868d727bad7135e
Mateusz Wojczal <mateusz@wojczal.com>
2022-10-03 17:19:57 +0200

    k8s

--- pull_init_k8s_generate.sh
+++ pull_init_k8s_generate.sh
@@ -4,4 +4,4 @@ cd EscolaLMS
 rm -rf .git
 make k8s-generate-yaml
 echo "Check k8s folder for your generated *.yaml files.";
-echo "Run k8b with command `kubectl apply -f k8s` ."
+echo "Run k8b with command 'kubectl apply -f k8s' ."
